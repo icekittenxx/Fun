dcae17e55e54ede57cbf5e54a6380a3042af06ab
XuXiang <icekittenxx@gmail.com>
2014-01-29 17:48:15 +0800

    PythonTip012 - 结尾非零数的奇偶性

---
+++ PythonTip/PythonTip012 - 结尾非零数的奇偶性.py
@@ -0,0 +1,21 @@
+m, k = 0, 0
+
+def main(n):
+    global m, k
+    if n % 2 == 0:
+        m += 1
+        main(n / 2)
+    elif n % 5 == 0:
+        k += 1
+        main(n / 5)
+            
+if __name__ == '__main__':
+    global m, k
+    
+    for i in L:
+        main(i)
+        
+    if m > k:
+        print 0
+    else:
+        print 1
